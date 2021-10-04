a351d5b442a742a24ab050c31313ab8b4d80b9d3
Charly95 <carlos.cmtz@hotmail.com>
2021-10-04 00:01:17 -0500

    Actualizacion_04_10_2021

---
+++ NetMarket/BussinesLogic/Logic/ProductoRepository.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinesLogic.Logic
+{
+    public class ProductoRepository : IProductoRepository
+    {
+        
+    }
+}
